0402e6a0bd5f6525c7d500683b4acaa621c41461
Lucas <lucasminneh270508@gmail.com>
2026-04-18 12:14:41 -0300

    Começando o Projeto

---
+++ principal.py
@@ -0,0 +1,7 @@
+import random as rd
+import time as tm 
+
+print("--------------------------------------------------------\n                                            \n ▗▄  ▄▄ ▗▄▄ ▗▄▄ ▗▖ ▖▄▄▄▖▗  ▖▗▄▄  ▗▖ ▗▖ ▖ ▄▄ \n▗▘ ▘▗▘▝▖▐ ▝▌ ▐  ▐▚ ▌ ▐  ▐  ▌ ▐   ▐▌ ▐▚ ▌▐▘ ▘\n▐   ▐  ▌▐▄▄▘ ▐  ▐▐▖▌ ▐  ▐▄▄▌ ▐   ▌▐ ▐▐▖▌▝▙▄ \n▐   ▐  ▌▐ ▝▖ ▐  ▐ ▌▌ ▐  ▐  ▌ ▐   ▙▟ ▐ ▌▌  ▝▌\n ▚▄▘ ▙▟ ▐  ▘▗▟▄ ▐ ▐▌ ▐  ▐  ▌▗▟▄ ▐  ▌▐ ▐▌▝▄▟▘\n                                            \n--------------------------------------------------------")
+print("\033[34m\nBem-Vindo ao Jogo do Time do Povo! \nJogo das Palavras!\033[34m")
+
+listaPalavras = ["Timão", "Fiel", "Itaquera", "Neo Química Arena", "Mosqueteiro", "Alvinegro", "Paulistão", "Brasileirão", "Libertadores", "Mundial", "Democracia", "Sócrates", "Rivais", "Derby", "Majestoso", "Alfredo Schürig", "Fazendinha", "Parque São Jorge", "Cássio", "Ronaldo", "Guerrero", "Tite", ]
